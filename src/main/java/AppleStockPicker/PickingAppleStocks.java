64968c182a895cb2af0c59db8cacfb96c5bddb16
MB <mbarry323@gmail.com>
2018-07-15 22:10:00 -0400

    shell for apple stock algo

---
+++ src/main/java/AppleStockPicker/PickingAppleStocks.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package AppleStockPicker;
+
+/**
+ *
+ * @author laptop
+ */
+public class PickingAppleStocks {
+
+    /**
+     * @param args the command line arguments
+     */
+//    public static void main(String[] args) {
+//        
+//        
+//    }
+    
+    public static int getMaxProfit(int[] stockPrices) {
+        
+    }
+    
+}
